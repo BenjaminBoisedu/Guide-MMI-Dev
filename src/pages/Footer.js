66259f9bc5e4681fa16be5fe65e015de9069a384
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2023-12-30 16:05:15 +0100

    footer css

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,5 +1,53 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="Footer">Footer</div>;
+  return (
+    <div className="Footer">
+      <p className="Name">© 2024 L'anti-sèche MMI</p>
+      <p className="auteur">
+        Site créé par{" "}
+        <a
+          href="
+    https://www.linkedin.com/in/benjamin-bois%C3%A9du-09434a241/"
+        >
+          Benjamin Boisédu
+        </a>
+      </p>
+      <div className="Copyright">
+        <p> Tout droits réservés à l'équipe de l'anti-sèche MMI </p>
+      </div>
+      <div className="Social">
+        <a
+          href="https://www.instagram.com/antisechemmi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png"
+            alt="instagram"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/anti-s%C3%A8che-mmi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/linkedin.png"
+            alt="linkedin"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/antisechemmi/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="https://img.icons8.com/ios-filled/50/000000/facebook.png"
+            alt="facebook"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,15 +9,17 @@ export default function Index() {
     <div className="Body-App">
       <div className="App">
         <header className="App-header">
-          <h1>Qu'est ce que l'anti-sèche MMI</h1>
-          <p className="Desc1">
-            L'anti-sèche MMI est un site qui permet aux étudiants de MMI de
-            trouver des anti-sèches pour les aider dans leurs études.
-          </p>
-          <p className="Desc2">
-            Attention ! Les anti-sèches ne sont pas à utiliser pour tricher mais
-            pour vous aider à comprendre.
-          </p>
+          <div className="Info">
+            <h1>Qu'est ce que l'anti-sèche MMI</h1>
+            <p className="Desc1">
+              L'anti-sèche MMI est un site qui permet aux étudiants de MMI de
+              trouver des anti-sèches pour les aider dans leurs études.
+            </p>
+            <p className="Desc2">
+              Attention ! Les anti-sèches ne sont pas à utiliser pour tricher
+              mais pour vous aider à comprendre.
+            </p>
+          </div>
           <div className="Catégories">
             {categories.map((catégorie, key) => (
               <div className="Catégorie" key={catégorie.Name}>
