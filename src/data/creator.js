619beb99a1d9e852dbf0b702342d57ca3821d380
BenjaminBoisedu <benjaminboisedu354@gmail.com>
2024-01-05 15:18:13 +0100

    fix bug creator img

--- src/data/creator.js
+++ src/data/creator.js
@@ -4,7 +4,7 @@ export const creator = {
   job: "Développeur web",
   etude: "BUT MMI",
   school: "IUT de Lens",
-  img: "https://media.licdn.com/dms/image/D4E35AQFI4hHMapJATw/profile-framedphoto-shrink_200_200/0/1697705725887?e=1704646800&v=beta&t=jMkffGmwmiFY0vABfqMHWUVP8nDh5zjejz4EiFqGKIA",
+  img: "https://avatars.githubusercontent.com/u/99210381?v=4",
   Linkedin: "https://www.linkedin.com/in/benjamin-bois%C3%A9du-09434a241/",
   Github: "https://github.com/BenjaminBoisedu",
   Instagram: "https://www.instagram.com/csenku_/",
